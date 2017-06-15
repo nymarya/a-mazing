f25d0dce455560a13decf3a4cbcad86aa70f38f7
GabrielArSouza <gabriel_feg@hotmail.com>
2017-06-15 16:58:45 -0300

    lendo apenas um lvl

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,49 @@
 #include <iostream>
 #include <cassert>
+#include "level.h"
 #include "maze.h"
-
-
-
- bool solve_maze( const Maze& mz, const Maze::Position pos){
- 	//Recupera
- 	const Maze::Position initial = mz.get_start_position();
-
- 	if( not mz.is_blocked())
-
- }
 
 int main(int arcg, char** argv){
 
-	Maze mz;
+	Level lvl;
 
-	//Validar argumentos
-
-	//Caregar labirinto
-	mz.load(argv[1]);
+	//Caregar e imprimir fase
+	lvl.load(argv[1]);
+	lvl.print_lvl();
+	
 	//if(solve_maze( mz, mz.get_start_position() ) ){
-		//soluçao encontrada
-	//}
-	auto pos = mz.get_start_position();
-	std::cout << pos.roll << std::endl;
-	std::cout << pos.col << std::endl;
+	// 	//soluçao encontrada
+	// //}
+	// auto pos = mz.get_start_position();
+	// std::cout << pos.roll << std::endl;
+	// std::cout << pos.col << std::endl;
 
-	pos.col+=1;
-	pos.roll-=1;
+	// pos.col+=1;
+	// pos.roll-=1;
 
-	mz.mark_cell(pos);
+	// mz.mark_cell(pos);
 
-	mz.render();
+	// mz.render();
 
-	Maze::Position p;
-	p.roll = 0;
-	p.col = 2;
+	// Maze::Position p;
+	// p.roll = 0;
+	// p.col = 2;
 
-	Maze::Direction d;
-	d.height = 0;
-	d.weight = 0;
+	// Maze::Direction d;
+	// d.height = 0;
+	// d.weight = 0;
 
-	assert ( mz.is_blocked(p, d));
+	// assert ( mz.is_blocked(p, d));
 
-	pos.roll-=1;
-	pos.col+=1;
-	mz.mark_cell(pos);
+	// pos.roll-=1;
+	// pos.col+=1;
+	// mz.mark_cell(pos);
 
-	assert( mz.is_marked(pos) == false);
+	// assert( mz.is_marked(pos) == false);
 
-	mz.unmark_cell(pos);
+	// mz.unmark_cell(pos);
 
-	assert( mz.is_marked(pos) == false);
+	// assert( mz.is_marked(pos) == false);
 
 	return 0;
 }
