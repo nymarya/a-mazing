9d204ad53e24eb5c2b5d74951027a249db155ae7
GabrielArSouza <gabriel_feg@hotmail.com>
2017-06-19 14:28:56 -0300

    código limpo e com solução

--- src/player.cpp
+++ src/player.cpp
@@ -70,9 +70,6 @@ bool Player::find_solution ( std::vector<std::string> & map )
 	//<! tabela hash para posições visitadas
 	HashTbl<Position, Move, KeyHash, KeyEqual> pos_visit(23);
 
-	//!< Pilha de decisões tomadas
-	// std::stack < Position> decisions;
-
 	Move x;
 	//<! pilha inicia com a posição inicial
 	possible_sol.push( inicial );
@@ -84,31 +81,11 @@ bool Player::find_solution ( std::vector<std::string> & map )
 	{	
 		// desempilha uma possível solução
 		x = possible_sol.top();
-		std::cout << "Possível solução\n";
-		std::cout << "l: " << x.pos.roll << " c: " << x.pos.col << std::endl;
 		possible_sol.pop();
-		std::cout << "SOLUTION: [ ";
-		auto cont(0);
-		for ( auto i = x.dir.begin(); i != x.dir.end(); ++i)
-		{
-			if ( *i == direction_t::NORTH ) std::cout << "N ";
-			else if ( *i == direction_t::SOUTH ) std::cout << "S ";
-			else if ( *i == direction_t::EAST ) std::cout << "E ";
-			else if ( *i == direction_t::WEST ) std::cout << "W ";
-			else if ( *i == direction_t::STATIC ) std::cout << "I ";
-
-			cont++;
-		}
-	std::cout << " ]\n";
 
 		///<! verifica se é a solução
 		if ( lvl.is_solution( x.pos ) )
-		{
-			std::cout << "Solução: l-" << x.pos.roll;
-			std::cout << " c-" << x.pos.col << std::endl;
-			
-			// for ( auto i((x.dir).begin()); i != (x.dir).end(); ++i)
-			// 	m_solution = (*i);	
+		{	
 			m_solution = x.dir;		
 
 			return true;
@@ -120,7 +97,6 @@ bool Player::find_solution ( std::vector<std::string> & map )
 			
 		} else
 		{
-			auto cont(0);
 			
 			for ( auto d  = (int) direction_t::NORTH; d != (int) direction_t::WEST +1; ++d )
 			{
@@ -130,91 +106,20 @@ bool Player::find_solution ( std::vector<std::string> & map )
 				Position direc;
 				direc.roll = de.height + x.pos.roll;
 				direc.col  = de.weight  + x.pos.col;
-				std::cout << "teste: " << direc.roll << " " << direc.col << std::endl;
 
 				Move mv;
 				//<! verificar se não é bloqueado
 				if ( !lvl.is_blocked( direc ) and not pos_visit.retrieve( direc, mv) )
 				{
 
-					std::cout << "achou possível solução\n";
-					std::cout << ">>>OLHA QUEM É D: " << d << std::endl;
 					Move ins;
 					ins.pos = direc; //de onde veio
 					ins.dir = x.dir;
-					std::cout << "X.dir: " << x.dir.size() << " ins " << ins.dir.size() << std::endl;
 					ins.dir.push_back(movements[ d ]); //direção que tomou
 					possible_sol.push( ins ); //adiciona move
-					cont++; //conta quantas soluções
-					auto test = possible_sol.top().dir;
-					std::cout << "ins [ ";
-					auto cont(0);
-					for ( auto i = ins.dir.begin(); i != ins.dir.end(); ++i)
-					{
-						if ( *i == direction_t::NORTH ) std::cout << cont << " NORTH,";
-						else if ( *i == direction_t::SOUTH ) std::cout << cont << " SOUTH,";
-						else if ( *i == direction_t::EAST ) std::cout << cont << " EAST,";
-						else if ( *i == direction_t::WEST ) std::cout << cont << " WEST,";
-						else if ( *i == direction_t::STATIC ) std::cout << cont << " Inicio,";
-						else std::cout << (int) *i << " ," ;
-
-						cont++;
-					}
-					std::cout << " ]	\n";
-					std::cout << "possible_sol [ ";
-					auto cont2(0);
-					for ( auto i = test.begin(); i != test.end(); ++i)
-					{
-						if ( *i == direction_t::NORTH ) std::cout << cont2 << " NORTH,";
-						else if ( *i == direction_t::SOUTH ) std::cout << cont2 << " SOUTH,";
-						else if ( *i == direction_t::EAST ) std::cout << cont2 << " EAST,";
-						else if ( *i == direction_t::WEST ) std::cout << cont2 << " WEST,";
-						else if ( *i == direction_t::STATIC ) std::cout << cont2 << " Inicio,";
-						else std::cout << (int) *i << " ," ;
-
-						cont2++;
-					}
-					std::cout << " ]	\n";
-					std::cout << "x [ ";
-					auto cont1(0);
-					for ( auto i = x.dir.begin(); i != x.dir.end(); ++i)
-					{
-						if ( *i == direction_t::NORTH ) std::cout << cont1 << " NORTH,";
-						else if ( *i == direction_t::SOUTH ) std::cout << cont1 << " SOUTH,";
-						else if ( *i == direction_t::EAST ) std::cout << cont1 << " EAST,";
-						else if ( *i == direction_t::WEST ) std::cout << cont1 << " WEST,";
-						else if ( *i == direction_t::STATIC ) std::cout << cont1 << " Inicio,";
-						else std::cout << (int) *i << " ," ;
-
-						cont1++;
-					}
-					std::cout << " ]	";
-															
-					if ( movements[ d ] == direction_t::NORTH ) std::cout << "N \n";
-					else if ( movements[ d ] == direction_t::SOUTH ) std::cout << "S \n";
-					else if ( movements[ d ]== direction_t::EAST ) std::cout << "E \n";
-					else if ( movements[ d ] == direction_t::WEST ) std::cout << "W \n";
-					else if ( movements[ d ] == direction_t::STATIC ) std::cout << "I \n";
+					
 				}   	
 			}
-
-			//<! verifica se é beco sem saída
-			// if ( cont == 0 )
-			// {
-			// 	//<! volta até a ultima decisão tomada
-			// 	auto point = Solution_.back();
-			// 	while ( point.pos != (decisions.top())) 
-			// 	{
-			// 		Solution_.pop_back();
-			// 		point = Solution_.back();
-			// 	}
-			// } 
-			// else if( cont > 1)
-			// {
-			// 	//!< Marca na pilha de decisoes
-			// 	decisions.push( x.pos);
-			// }
-			cont = 0;
 		}
 	}
 	return false;
